650b11f884adc05e4dda83570830edb5d7d9a0a7
matador108117 <zS23004741@estudiantes.uv.mx>
2025-05-16 13:50:32 -0600

    initial fix

--- src/main/java/com/example/joshi/domain/factory/OpenWheaterFactory.java
+++ src/main/java/com/example/joshi/domain/factory/OpenWheaterFactory.java
@@ -1,5 +1,11 @@
 package com.example.joshi.domain.factory;
+import com.example.joshi.logic.OpenWeatherProvider;
+import com.example.joshi.domain.WeatherProvider;
 
-public class OpenWheaterFactory {
+public class OpenWheaterFactory implements WeatherProviderFactory {
+    @Override
+    public WeatherProvider creaProvider() {
+        return new OpenWeatherProvider();
+    }
     
 }
--- src/main/java/com/example/joshi/domain/factory/WeatherProviderFactory.java
+++ src/main/java/com/example/joshi/domain/factory/WeatherProviderFactory.java
@@ -5,13 +5,6 @@ import com.example.joshi.logic.OpenWeatherProvider;
 import com.example.joshi.logic.WeatherAPIProvider;
 import com.example.joshi.logic.WeatherbitProvider;
 
-public class WeatherProviderFactory {
-    public static WeatherProvider getProvider(String providerName) {
-        return switch (providerName.toLowerCase()) {
-            case "openweather" -> new OpenWeatherProvider();
-            case "weatherapi" -> new WeatherAPIProvider();
-            case "weatherbit" -> new WeatherbitProvider();
-            default -> throw new IllegalArgumentException("Proveedor no válido");
-        };
-    }
+public interface WeatherProviderFactory {
+    public WeatherProvider creaProvider();
 }
