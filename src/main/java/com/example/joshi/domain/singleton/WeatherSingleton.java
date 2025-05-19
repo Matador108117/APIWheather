0d2ac2757cbe9cd8434c4c5fdf9fa54384acdf79
TooVerletzt <zS23004724@estudiantes.uv.mx>
2025-05-19 17:23:52 -0600

    campo de juegoss

--- src/main/java/com/example/joshi/domain/singleton/WeatherSingleton.java
+++ src/main/java/com/example/joshi/domain/singleton/WeatherSingleton.java
@@ -7,12 +7,11 @@ public class WeatherSingleton {
     private static WeatherSingleton instance;
     private final WeatherDirector director;
 
-    // Constructor privado
+    
     private WeatherSingleton() {
         director = new WeatherDirector();
     }
-
-    // Método estático de acceso global
+   
     public static WeatherSingleton getInstance() {
         if (instance == null) {
             instance = new WeatherSingleton();
